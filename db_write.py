79e1157478c724687ee414e4da2cbb2dc0d9b176
Pavankumar Suresh <Pavancseds@gmail.com>
2025-12-19 13:16:46 -0800

    Modified db_write to have the environment variables defaults

--- db_write.py
+++ db_write.py
@@ -1,28 +1,32 @@
 import psycopg
 import pandas as pd
-
-conn = psycopg.connect(
-    host="localhost",
-    port=5432,
-    dbname="funding_db",
-    user="pavankumar_s"
-)
-
+import os
 
 def write_to_db(df):
-    df["timestamp"] = pd.to_datetime(df["timestamp"], utc=True)
-    records = list(df.itertuples(index=False, name=None))
-    insert_sql = """
-    INSERT INTO funding_rates (timestamp, symbol, rate)
-    VALUES (%s, %s, %s)
-    ON CONFLICT (timestamp, symbol) DO NOTHING;
-    """
-    with conn.cursor() as cur:
-        cur.executemany(insert_sql, records)
-        inserted = cur.rowcount
+    conn = psycopg.connect(
+        host=os.getenv("DB_HOST", "localhost"),
+        port=5432,
+        dbname=os.getenv("DB_NAME", "funding_db"),
+        user=os.getenv("DB_USER", "pavankumar_s"),
+        password=os.getenv("DB_PASSWORD")
+    )
 
-    conn.commit()
-    conn.close()
+    try:
+        df["timestamp"] = pd.to_datetime(df["timestamp"], utc=True)
+        records = list(df.itertuples(index=False, name=None))
 
-    print(f"Inserted {inserted} records")
-    return inserted
+        insert_sql = """
+        INSERT INTO funding_rates (timestamp, symbol, rate)
+        VALUES (%s, %s, %s)
+        ON CONFLICT (timestamp, symbol) DO NOTHING;
+        """
+
+        with conn.cursor() as cur:
+            cur.executemany(insert_sql, records)
+            inserted = cur.rowcount
+
+        conn.commit()
+        return inserted
+
+    finally:
+        conn.close()
